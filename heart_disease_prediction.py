a5b84d36383c50cd64f0be551d3bcd08cf88c724
Nyanda Freddy <87903153+nyandajr@users.noreply.github.com>
2023-02-02 19:00:26 +0300

    Update heart_disease_prediction.py

--- heart_disease_prediction.py
+++ heart_disease_prediction.py
@@ -1,64 +1,91 @@
-import streamlit as st
+import streamlit as st 
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-heart_data = pd.read_csv("heart_disease_data.csv")
+df = pd.read_csv("heart_disease_data.csv")
 
-# Split data into features and target
-X = heart_data.drop(columns='target',axis=1)
-Y = heart_data['target']
 
-# Split data into training and test sets
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y,random_state=2)
+X = df.drop(columns='target',axis = 1)
+Y = df['target']
 
-# Train logistic regression model
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.1, stratify = Y, random_state = 2)
+
 model = LogisticRegression()
 model.fit(X_train, Y_train)
 
-# Get accuracy of model on training and test data
+#Accuracy on training data
 X_train_prediction = model.predict(X_train)
 training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+
+
+# Accuracy on testing data 
 X_test_prediction = model.predict(X_test)
-test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
-
-# Create UI elements for inputting data and displaying results
-st.markdown("<h1 style='text-align:center;'>Heart Disease AI Prediction App</h1>", unsafe_allow_html=True)
+testing_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 
 
 
-# heart_img = "heart.webp"
-# st.image(heart_img, use_column_width=300)
+# Creating UI elements for data input
+st.markdown("<h1 style=align:center;'>Heart Disease AI Prediction App</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style=align:center;'>Please enter the data input of your patient </h3>", unsafe_allow_html=True)
 
-input_data = st.sidebar.text_input('''Enter patient data in the following order--> 
-                        age	,sex, cp,	trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal:''')
+age = st.number_input("age", value=0, step=1)
+sex = st.selectbox("sex", [0, 1])
+cp = st.number_input("cp", value=0, step=1)
+trestbps = st.number_input("trestbps", value=0, step=1)
+chol = st.number_input("chol", value=0, step=1)
+fbs = st.number_input("fbs", value=0, step=1)
+restecg = st.number_input("restecg", value=0, step=1)
+thalach = st.number_input("thalach", value=0, step=1)
+exang = st.number_input("exang", value=0, step=1)
+oldpeak = st.number_input("oldpeak", value=0, step=1)
+slope = st.number_input("slope", value=0, step=1)
+ca = st.number_input("ca", value=0, step=1)
+thal = st.number_input("thal", value=0, step=1)
 
-if input_data:
-    if len(input_data.split(',')) != 13:
-        st.markdown("<h2 style='text-align:center;color:red;'>Please enter 13 columns of data only.</h2>", unsafe_allow_html=True)
 
+
+button = st.button("Predict")
+if button:
+    user_data = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+    prediction = model.predict([user_data])
+    if prediction == 0:
+       st.markdown("<h4 style=align:center;color=green'>The patient does not have heart issues</45>", unsafe_allow_html=True)
+       
     else:
-        input_data_as_numpy_array = np.asarray(list(map(float, input_data.split(','))))
-        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-        prediction = model.predict(input_data_reshaped)
+        st.markdown("<h4 style=align:center;color=red'>The patient does not have heart issues</h4>", unsafe_allow_html=True)
+        
+        st.write(f"Accuracy on training data is {training_data_accuracy}")
+        st.write(f"Accuracy on testing data is {testing_data_accuracy}")
+        
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
 
-        st.write("Accuracy on Training data : ", training_data_accuracy)
-        st.write("Accuracy on Testing data : ", test_data_accuracy)
-        if prediction[0] == 0:
-            st.write("The machine confirms that person does not have a heart disease.")
-        else:
-            st.write("The machine confirms that person has a heart disease.")
-st.empty()
-st.empty()
-st.empty()
-st.empty()
-st.empty()
-st.empty()
-st.empty()
-st.empty()
-st.empty()
-st.empty()        
-st.empty()
-st.markdown("<h6 style='text-align:center;color:yellow;font-weight:bold;font-family:Arial;'>Developed by Nyanda Jr</h6>", unsafe_allow_html=True)
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Coyright @Nyanda Jr</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
